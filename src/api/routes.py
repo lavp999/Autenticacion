0e8766ba6745a2c06134a5522efbf2e0124fc79d
lavp999 <lavp99@hotmail.com>
2022-12-05 19:31:42 +0000

    arrancado

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,67 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/signup', methods=['POST'])
+def login():
+    data = request.json
+
+    emailUser = data.get("user", None)
+    pwd = data.get("pwd", None)
+
+    user = User.query.filter_by(email=emailUser).filter_by(password=pwd).first()
+
+    if user:
+        access_token = create_access_token(identity=emailUser)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.json
+
+    emailUser = data.get("user", None)
+    pwd = data.get("pwd", None)
+
+    user = User.query.filter_by(email=emailUser).filter_by(password=pwd).first()
+
+    if user:
+        access_token = create_access_token(identity=emailUser)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
