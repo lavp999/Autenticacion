cbfab618d029b71187b7889ab9566feeb0dcc3d4
lavp999 <lavp99@hotmail.com>
2023-01-10 14:00:10 +0000

    Con José Monzon

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -26,7 +26,6 @@ export const Login = () => {
 		.then((response)=>{	console.log("hacerLogin", typeof response["token"], response)
 			  				if(typeof response["token"] === 'undefined'){
 								actions.setMensaje("Mi mensaje");
-								navigate("/login");
 							}else{
 								localStorage.setItem("token", response["token"]);
 								actions.setUserConectado(response["nombre"], response["token"]);
@@ -54,13 +53,37 @@ export const Login = () => {
 					<input type="checkbox" className="form-check-input" id="exampleCheck1"/>
 					<label className="form-check-label" for="exampleCheck1">Check me out</label>
 				</div>
-				<button type="submit" className="btn btn-primary">Login</button>
+				<button type="submit" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Login</button>
 			</form>
 
 			<Link to="/" type="button" className="btn btn-success mx-3">Home</Link>
 
 			<div className="alert alert-info">
 			</div>
+
+
+<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        ...
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
 		</div>
 	);
 };
